cbb807c15f4d06986fe45779e359aa8243f3f3e9
Titus Mumo <titusmumo@gmail.com>
2022-08-29 10:53:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length; i++) {
+    const abba = word.length -1
+
+    if (word[i] === word [abba]) {
+      return true
+    }else {
+      return false
+    }
+  }
 }
 
 /* 
-  Add your pseudocode here
+  iterate through a word 
+  compare the words being iterated over the corresponding letter at the end of the string"
+  if letter match return true
 */
 
 /*
-  Add written explanation of your solution here
+  the function iterates from the beginning of the word 
+  compare the letter being iterated over the corresponding letter
+  if the letter match return true
+  if letter do not match return false
+
 */
 
 // You can run `node index.js` to view these console logs
